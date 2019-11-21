76da76ef59879a51b09a94fd9cb2dad16918e662
Nir <nir61202@gmail.com>
2019-11-21 15:06:38 +0200

    Add proto definition and build scripts for protobufs

---
+++ build/buildModel.sh
@@ -0,0 +1,3 @@
+# Uncomment the next line to download the protobuf Go compiler. Note that $GOPATH/bin must be in PATH.
+# go get -u github.com/golang/protobuf/protoc-gen-go
+protoc --go_out=plugins=grpc:./ model/model.proto
