bc9bad10a46e45f10329508aede05cfdf1a2f823
Nir <nir61202@gmail.com>
2020-03-22 02:59:32 +0200

    Update verifier.go

--- core/verifier.go
+++ core/verifier.go
@@ -5,12 +5,10 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"hash"
-	"io"
-	"os"
-
 	"github.com/plutoberth/Failsystem/crypto/strlen"
 	pb "github.com/plutoberth/Failsystem/model"
+	"hash"
+	"io"
 )
 
 //VerifyDataHash checks if the response matches the file.
@@ -42,17 +40,4 @@ func VerifyDataHash(resp *pb.DataHash, file io.ReadSeeker) (bool, error) {
 
 	//Is the hash valid?
 	return hex.EncodeToString(hasher.Sum(nil)) == resp.GetHexHash(), nil
-}
-
-func CreateDataHash(file *os.File) (*pb.DataHash, error) {
-	var hasher hash.Hash = sha256.New()
-
-	if _, err := file.Seek(0, io.SeekStart); err != nil {
-		return nil, err
-	}
-
-	if _, err := io.Copy(hasher, file); err != nil {
-		return nil, err
-	}
-	return &pb.DataHash{Type: pb.HashType_SHA256, HexHash: hex.EncodeToString(hasher.Sum(nil))}, nil
 }
