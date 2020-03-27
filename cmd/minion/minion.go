faae40511755a2397301d7b7ad467f9fad0ce085
Nir <nir61202@gmail.com>
2020-03-27 17:24:11 +0300

    Add methods to finalize file creations and find servers with enough space

--- cmd/minion/minion.go
+++ cmd/minion/minion.go
@@ -7,7 +7,7 @@ import (
 )
 
 var port = flag.Uint("port", 31337, "The Server's port.")
-var quota = flag.Int64("quota", 13371337, "The data folder's quota.")
+var quota = flag.Int64("quota", 10000, "The data folder's quota.")
 var folderPath = flag.String("folder", "./dataFolder", "The data folder's path.")
 
 func main() {
