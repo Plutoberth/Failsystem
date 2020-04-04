abc01f6f68aabda4b75e3cad5bf9b09db17f4bdd
Nir <nir61202@gmail.com>
2020-04-04 20:11:25 +0300

    Add a way to list files to the db

--- core/master/masterdb/db.go
+++ core/master/masterdb/db.go
@@ -11,11 +11,11 @@ type Datastore interface {
 	UpdateServerEntry(ctx context.Context, entry ServerEntry) error
 	GetServerEntry(ctx context.Context, UUID string) (*ServerEntry, error)
 	GetServersWithEnoughSpace(ctx context.Context, requestedSize int64) ([]ServerEntry, error)
-
 	CreateFileEntry(ctx context.Context, entry FileEntry) error
 	UpdateFileHosts(ctx context.Context, fileUUID string, serverUUID string) error
 	FinalizeFileEntry(ctx context.Context, fileUUID string, hash pb.DataHash) error
 	GetFileEntry(ctx context.Context, UUID string) (*FileEntry, error)
+	ListFiles(ctx context.Context, UUID string) ([]FileEntry, error)
 }
 
 type ServerEntry struct {
